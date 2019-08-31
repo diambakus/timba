524d719e5d3f572b571f809199b1664119545cee
diambakus <lazoss@gmail.com>
2019-08-31 04:51:37 -0300

    improving...

--- DBConn.py
+++ DBConn.py
@@ -1,8 +1,27 @@
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
+from configparser import ConfigParser
 
 
-engine = sqlalchemy.create_engine('mysql+mysqlconnector://timba:timbapa55@localhost/timbadb', echo=True)
+def read_database_config(filename='config.ini', section='mysql'):
+    parser = ConfigParser()
+    parser.read(filename)
+
+    # get section, default to mysql
+    db = {}
+    if parser.has_section(section):
+        items = parser.items(section)
+        for item in items:
+            db[item[0]] = item[1]
+    else:
+        raise Exception('{0} not found in the {1} file'.format(section, filename))
+
+    return db
+
+
+config_db = read_database_config()
+engine = sqlalchemy.create_engine('mysql+mysqlconnector://'+config_db['user']+':'+config_db['password']+'@'+
+                                  config_db['host']+'/'+config_db['database'], echo=True)
 
 
 def get_engine():
